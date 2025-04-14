20ed17781b64c1767bb86860a2d6555ccbe956ed
Ranjeet Kumar <ranjeetkumar.dev2@gmail.com>
2025-04-14 23:23:17 +0530

    added Service class for Appwrite integration

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,6 @@ import conf from "./conf/conf";
 import authService from "./appwrite/auth";
 
 function App() {
-  // (async () => {
-  //   try {
-  //     const user = await authService.createAccount({
-  //       name: "ranjeet",
-  //       email: "ranjeetkumargond000@gmail.com",
-  //       password: "123456789",
-  //     });
-  //     console.log(user);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // })();
   return <div>App</div>;
 }
 
