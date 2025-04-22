d99ba8e068b8434878b55c97de2ce1cfbd448f3f
Ranjeet Kumar <ranjeetkumar.dev2@gmail.com>
2025-04-22 23:50:42 +0530

    Add Protected route, Login, Signup, and PostForm components with authentication and form handling

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import Logout from "./Headers/Logout";
-export { Header, Footer, Container, Logo, Logout };
+import RTE from "./RTE";
+export { Header, Footer, Container, Logo, Logout, RTE };
